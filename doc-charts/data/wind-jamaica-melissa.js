78a4948ebe856a7930082c0bbd96ee67aa989607
Mark Young <71027206+guiballs415@users.noreply.github.com>
2025-11-24 12:37:48 -0800

    Add files via upload

---
+++ doc-charts/data/wind-jamaica-melissa.js
@@ -0,0 +1,155 @@
+// Time-series per Jamaican parish for wind speeds due to Hurricane Melissa
+// Generated from data from Google Cyclones forecasts with
+// file:///Users/markyoung/Documents/Google/health/src/gsr-gemini/data/hurricane/hurricane-data.html
+var wind_speed_timeseries = [
+{ parish: "Clarendon", timeseries: [
+{ day: "Mon", date: "Oct 27", time: "1900", hours: 21, wind: 39 },
+{ day: "Mon", date: "Oct 27", time: "2200", hours: 24, wind: 39 },
+{ day: "Tue", date: "Oct 28", time: "0100", hours: 27, wind: 40 },
+{ day: "Tue", date: "Oct 28", time: "0400", hours: 30, wind: 43 },
+{ day: "Tue", date: "Oct 28", time: "0700", hours: 33, wind: 47 },
+{ day: "Tue", date: "Oct 28", time: "1000", hours: 36, wind: 51 },
+{ day: "Tue", date: "Oct 28", time: "1300", hours: 39, wind: 54 },
+{ day: "Tue", date: "Oct 28", time: "1600", hours: 42, wind: 55 },
+{ day: "Tue", date: "Oct 28", time: "1900", hours: 45, wind: 57 }, // 57 max
+{ day: "Tue", date: "Oct 28", time: "2200", hours: 48, wind: 53 }
+] },
+{ parish: "Hanover", timeseries: [
+{ day: "Tue", date: "Oct 28", time: "1000", hours: 36, wind: 45 },
+{ day: "Tue", date: "Oct 28", time: "1300", hours: 39, wind: 52 },
+{ day: "Tue", date: "Oct 28", time: "1600", hours: 42, wind: 65 },
+{ day: "Tue", date: "Oct 28", time: "1900", hours: 45, wind: 71 }, // 71 max
+{ day: "Tue", date: "Oct 28", time: "2200", hours: 48, wind: 42 }
+] },
+{ parish: "Kingston", timeseries: [
+{ day: "Mon", date: "Oct 27", time: "1300", hours: 15, wind: 39 },
+{ day: "Mon", date: "Oct 27", time: "1600", hours: 18, wind: 39 },
+{ day: "Mon", date: "Oct 27", time: "1900", hours: 21, wind: null },
+{ day: "Mon", date: "Oct 27", time: "2200", hours: 24, wind: null },
+{ day: "Tue", date: "Oct 28", time: "0100", hours: 27, wind: null },
+{ day: "Tue", date: "Oct 28", time: "0400", hours: 30, wind: 40 },
+{ day: "Tue", date: "Oct 28", time: "0700", hours: 33, wind: 43 },
+{ day: "Tue", date: "Oct 28", time: "1000", hours: 36, wind: 46 },
+{ day: "Tue", date: "Oct 28", time: "1300", hours: 39, wind: 48 },
+{ day: "Tue", date: "Oct 28", time: "1600", hours: 42, wind: 48 },
+{ day: "Tue", date: "Oct 28", time: "1900", hours: 45, wind: 50 }, // 50 max
+{ day: "Tue", date: "Oct 28", time: "2200", hours: 48, wind: 51 },
+{ day: "Wed", date: "Oct 29", time: "0100", hours: 51, wind: 42 }
+] },
+{ parish: "Manchester", timeseries: [
+{ day: "Mon", date: "Oct 27", time: "2200", hours: 24, wind: 39 },
+{ day: "Tue", date: "Oct 28", time: "0100", hours: 27, wind: 41 },
+{ day: "Tue", date: "Oct 28", time: "0400", hours: 30, wind: 44 },
+{ day: "Tue", date: "Oct 28", time: "0700", hours: 33, wind: 48 },
+{ day: "Tue", date: "Oct 28", time: "1000", hours: 36, wind: 53 },
+{ day: "Tue", date: "Oct 28", time: "1300", hours: 39, wind: 57 },
+{ day: "Tue", date: "Oct 28", time: "1600", hours: 42, wind: 63 },
+{ day: "Tue", date: "Oct 28", time: "1900", hours: 45, wind: 66 }, // 66 max
+{ day: "Tue", date: "Oct 28", time: "2200", hours: 48, wind: 50 }
+] },
+{ parish: "Portland", timeseries: [
+{ day: "Tue", date: "Oct 28", time: "1000", hours: 36, wind: 42 },
+{ day: "Tue", date: "Oct 28", time: "1300", hours: 39, wind: 44 },
+{ day: "Tue", date: "Oct 28", time: "1600", hours: 42, wind: 45 },
+{ day: "Tue", date: "Oct 28", time: "1900", hours: 45, wind: 47 },
+{ day: "Tue", date: "Oct 28", time: "2200", hours: 48, wind: 50 }, // 50 max
+{ day: "Wed", date: "Oct 29", time: "0100", hours: 51, wind: 48 }
+] },
+{ parish: "St Andrew", timeseries: [
+{ day: "Tue", date: "Oct 28", time: "0400", hours: 30, wind: 40 },
+{ day: "Tue", date: "Oct 28", time: "0700", hours: 33, wind: 42 },
+{ day: "Tue", date: "Oct 28", time: "1000", hours: 36, wind: 45 },
+{ day: "Tue", date: "Oct 28", time: "1300", hours: 39, wind: 47 },
+{ day: "Tue", date: "Oct 28", time: "1600", hours: 42, wind: 48 },
+{ day: "Tue", date: "Oct 28", time: "1900", hours: 45, wind: 50 },
+{ day: "Tue", date: "Oct 28", time: "2200", hours: 48, wind: 52 }, // 52 max
+{ day: "Wed", date: "Oct 29", time: "0100", hours: 51, wind: 44 }
+] },
+{ parish: "St Ann", timeseries: [
+{ day: "Tue", date: "Oct 28", time: "0400", hours: 30, wind: 40 },
+{ day: "Tue", date: "Oct 28", time: "0700", hours: 33, wind: 43 },
+{ day: "Tue", date: "Oct 28", time: "1000", hours: 36, wind: 48 },
+{ day: "Tue", date: "Oct 28", time: "1300", hours: 39, wind: 52 },
+{ day: "Tue", date: "Oct 28", time: "1600", hours: 42, wind: 55 },
+{ day: "Tue", date: "Oct 28", time: "1900", hours: 45, wind: 57 },
+{ day: "Tue", date: "Oct 28", time: "2200", hours: 48, wind: 67 },  // 67 max
+{ day: "Wed", date: "Oct 29", time: "0100", hours: 51, wind: 46 }
+] },
+{ parish: "St Catherine", timeseries: [
+{ day: "Mon", date: "Oct 27", time: "1600", hours: 18, wind: 39 },
+{ day: "Mon", date: "Oct 27", time: "1900", hours: 21, wind: 40 },
+{ day: "Mon", date: "Oct 27", time: "2200", hours: 24, wind: null },
+{ day: "Tue", date: "Oct 28", time: "0100", hours: 27, wind: 39 },
+{ day: "Tue", date: "Oct 28", time: "0400", hours: 30, wind: 42 },
+{ day: "Tue", date: "Oct 28", time: "0700", hours: 33, wind: 45 },
+{ day: "Tue", date: "Oct 28", time: "1000", hours: 36, wind: 48 },
+{ day: "Tue", date: "Oct 28", time: "1300", hours: 39, wind: 51 },
+{ day: "Tue", date: "Oct 28", time: "1600", hours: 42, wind: 52 },
+{ day: "Tue", date: "Oct 28", time: "1900", hours: 45, wind: 53 }, // 53 max
+{ day: "Tue", date: "Oct 28", time: "2200", hours: 48, wind: 53 },
+{ day: "Wed", date: "Oct 29", time: "0100", hours: 51, wind: 41 }
+] },
+{ parish: "St Elizabeth", timeseries: [
+{ day: "Tue", date: "Oct 28", time: "0100", hours: 27, wind: 40 },
+{ day: "Tue", date: "Oct 28", time: "0400", hours: 30, wind: 44 },
+{ day: "Tue", date: "Oct 28", time: "0700", hours: 33, wind: 48 },
+{ day: "Tue", date: "Oct 28", time: "1000", hours: 36, wind: 53 },
+{ day: "Tue", date: "Oct 28", time: "1300", hours: 39, wind: 61 },
+{ day: "Tue", date: "Oct 28", time: "1600", hours: 42, wind: 91 }, // 91 max
+{ day: "Tue", date: "Oct 28", time: "1900", hours: 45, wind: 74 },
+{ day: "Tue", date: "Oct 28", time: "2200", hours: 48, wind: 46 }
+] },
+{ parish: "St James", timeseries: [
+{ day: "Tue", date: "Oct 28", time: "0700", hours: 33, wind: 39 },
+{ day: "Tue", date: "Oct 28", time: "1000", hours: 36, wind: 45 },
+{ day: "Tue", date: "Oct 28", time: "1300", hours: 39, wind: 52 },
+{ day: "Tue", date: "Oct 28", time: "1600", hours: 42, wind: 64 },
+{ day: "Tue", date: "Oct 28", time: "1900", hours: 45, wind: 141 }, // 141 max
+{ day: "Tue", date: "Oct 28", time: "2200", hours: 48, wind: 50 }
+] },
+{ parish: "St Mary", timeseries: [
+{ day: "Tue", date: "Oct 28", time: "0700", hours: 33, wind: 42 },
+{ day: "Tue", date: "Oct 28", time: "1000", hours: 36, wind: 45 },
+{ day: "Tue", date: "Oct 28", time: "1300", hours: 39, wind: 48 },
+{ day: "Tue", date: "Oct 28", time: "1600", hours: 42, wind: 50 },
+{ day: "Tue", date: "Oct 28", time: "1900", hours: 45, wind: 52 },
+{ day: "Tue", date: "Oct 28", time: "2200", hours: 48, wind: 55 }, // 55 max
+{ day: "Wed", date: "Oct 29", time: "0100", hours: 51, wind: 49 }
+] },
+{ parish: "St Thomas", timeseries: [
+{ day: "Sun", date: "Oct 26", time: "2200", hours: 0, wind: 39 },
+{ day: "Mon", date: "Oct 27", time: "0100", hours: 3, wind: 40 },
+{ day: "Mon", date: "Oct 27", time: "0400", hours: 6, wind: null },
+{ day: "Mon", date: "Oct 27", time: "0700", hours: 9, wind: null },
+{ day: "Mon", date: "Oct 27", time: "1000", hours: 12, wind: null },
+{ day: "Mon", date: "Oct 27", time: "1300", hours: 15, wind: null },
+{ day: "Mon", date: "Oct 27", time: "1600", hours: 18, wind: null },
+{ day: "Mon", date: "Oct 27", time: "1900", hours: 21, wind: null },
+{ day: "Mon", date: "Oct 27", time: "2200", hours: 24, wind: null },
+{ day: "Tue", date: "Oct 28", time: "0100", hours: 27, wind: null },
+{ day: "Tue", date: "Oct 28", time: "0400", hours: 30, wind: null },
+{ day: "Tue", date: "Oct 28", time: "0700", hours: 33, wind: null },
+{ day: "Tue", date: "Oct 28", time: "1000", hours: 36, wind: 41 },
+{ day: "Tue", date: "Oct 28", time: "1300", hours: 39, wind: 43 },
+{ day: "Tue", date: "Oct 28", time: "1600", hours: 42, wind: 43 },
+{ day: "Tue", date: "Oct 28", time: "1900", hours: 45, wind: 45 },
+{ day: "Tue", date: "Oct 28", time: "2200", hours: 48, wind: 48 }, // 48 max
+{ day: "Wed", date: "Oct 29", time: "0100", hours: 51, wind: 44 }
+] },
+{ parish: "Trelawny", timeseries: [
+{ day: "Tue", date: "Oct 28", time: "0400", hours: 30, wind: 39 },
+{ day: "Tue", date: "Oct 28", time: "0700", hours: 33, wind: 44 },
+{ day: "Tue", date: "Oct 28", time: "1000", hours: 36, wind: 49 },
+{ day: "Tue", date: "Oct 28", time: "1300", hours: 39, wind: 54 },
+{ day: "Tue", date: "Oct 28", time: "1600", hours: 42, wind: 64 },
+{ day: "Tue", date: "Oct 28", time: "1900", hours: 45, wind: 133 }, // 133 max
+{ day: "Tue", date: "Oct 28", time: "2200", hours: 48, wind: 60 }
+] },
+{ parish: "Westmoreland", timeseries: [
+{ day: "Tue", date: "Oct 28", time: "0700", hours: 33, wind: 43 },
+{ day: "Tue", date: "Oct 28", time: "1000", hours: 36, wind: 49 },
+{ day: "Tue", date: "Oct 28", time: "1300", hours: 39, wind: 57 },
+{ day: "Tue", date: "Oct 28", time: "1600", hours: 42, wind: 98 }, // 98 max
+{ day: "Tue", date: "Oct 28", time: "1900", hours: 45, wind: 71 }
+] }
+];
